ed19ee954a264a3874a0802fa8f3696f9663dfd6
jahidhasanco <vdjsovaj@gmail.com>
2022-11-12 07:35:12 +0600

    update library

--- app/src/main/java/dev/jahidhasanco/sample/MainActivity.kt
+++ app/src/main/java/dev/jahidhasanco/sample/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+
         binding.pullDownAnimateRefreshLayout.setOnRefreshListener(object :
             PullDownAnimateRefreshLayout.OnRefreshListener {
             override fun onRefresh() {
